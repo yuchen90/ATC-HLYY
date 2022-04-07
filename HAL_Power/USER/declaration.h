f14599962d4c257a96477eeef394402199b3d5d4
Yuchen Hou <realhouyuchen@gmail.com>
2022-04-07 17:22:42 +0800

    电源板，修改外设配置，串口用DMA收发，CAN用硬件过滤分配 SID到FIFO0, EXID到FIFO1。主函数写好，但是引用函数与外设中断未编写。

--- HAL_Power/USER/declaration.h
+++ HAL_Power/USER/declaration.h
@@ -3,10 +3,22 @@
 //extern 引用 global_variable.h 声明的函数或全局变量
 
 #include "stdint.h"
+#include "stm32f1xx_hal.h"
 
+//外设句柄
+    extern UART_HandleTypeDef POWER_UART2_Handle;
+    extern uint8_t UART_Rx_Buff[UART2_RxDataSize];
+    extern DMA_HandleTypeDef UART2Rx_DMA_Handle;
+    extern DMA_HandleTypeDef UART2Tx_DMA_Handle;
+
+    extern CAN_HandleTypeDef CAN_Handle;
+    extern CAN_TxHeaderTypeDef CAN_TxHeader;
+    extern CAN_RxHeaderTypeDef CAN_RxHeader;
+
+    extern TIM_HandleTypeDef TIM2_Handle;
 //系统变量
-    extern uint32_t SYSTEM_Time_Fg;                                     //TIM2中断（100us一次）++； TFg(TimeFlag 缩写)
-    extern uint32_t SYSTEM_Time_Fg1;                                    //灯控板闪烁先亮后灭时，置0.
+    extern uint32_t System_Time_Fg;                                      //TIM2中断（100us一次）++； TFg(TimeFlag 缩写)
+    extern uint32_t System_Time_Fg1;                                     //灯控板闪烁先亮后灭时，置0.
     extern uint32_t CAN_DataRead_Fg;                                    //主控板发给灯控板发送点灯数据，电源板接收到的标识符
 
 // CAN消息buff
@@ -16,10 +28,10 @@
     
     //灯控板点灯信息
     extern uint8_t CAN_Buff[16];                                        //两包主控板发给灯控板点灯数据接收缓存
-    extern uint8_t cmd[15];                                             //接管信息 点灯指令
-    extern uint8_t Blink_Id_Now;                                        //点灯信息，闪烁灯是否亮起标识， 0：灭； 1：亮
+    extern uint8_t CMD[15];                                             //接管信息 点灯指令
+    extern uint8_t Blink_Id;                                            //点灯信息，闪烁灯是否亮起标识， 0：灭； 1：亮
     extern uint8_t Blink_Id_Before;                                     //上一次闪烁标识
-    extern uint8_t Opt_CheckReply_Buff;                                 //灯控板点灯反馈标识，缓存
+    extern uint8_t Opt_CheckReply_Fg;                                   //灯控板点灯反馈标识，缓存
     extern uint8_t Opt_CheckReply_1st;                                  //第一包数据灯控板点灯反馈标识
     extern uint8_t Opt_CheckReply_2nd;                                  //第二包数据灯控板点灯反馈标识
     extern uint8_t Opt_BoardId;                                         //灯控板编号
@@ -42,7 +54,7 @@
     extern uint8_t JSY_MK163_ReadBuff[20];                              //市电感应读取数据缓存
     extern uint8_t JSY_MK163_ReadNum;                                   //市电感应读取数据 元素索引
     extern uint16_t JSY_MK163_ReadRequest_Fg;                           //市电感应信息查询动作标识， 每秒查询1次，每次接收完整点灯信息帧加1，大于等于4时，置0
-    extern uint8_t SendBuff[8];
+    extern uint8_t UART_Send_Buff[UART2_TxDataSize];
 
     //检测信息(主控板->电源板)
     extern uint8_t TakeOver_Enable;                                     //是否需要接管
@@ -62,11 +74,6 @@
     extern uint16_t YBlink_Count4;                                      //非硬黄闪计数
     extern uint8_t YBlink_Fg;                                           //
 
-//外设初始化错误处理函数声明
-    
-    extern void Error_Handler(void);
-
 //DEBUG 参数
-    extern uint32_t e,t,r;
-
+    extern uint32_t w,e,t,r;
 #endif
--- HAL_Power/USER/main.h
+++ HAL_Power/USER/main.h
@@ -3,10 +3,54 @@
 #define __MAIN_H__
 //main.h 宏定义与结构体声明 
 #include "stm32f1xx_hal.h"
-#include "sys.h"
-//stm32f1xx_hal.h 引用，因为一些外设名字宏定义修改，方便移植
-//sys.h 引用，stm32 位带操作
 
+//位带操作,实现51类似的GPIO控制功能
+//具体实现思想,参考<<CM3权威指南>>第五章(87页~92页).
+//IO口操作宏定义
+#define BITBAND(addr, bitnum) ((addr & 0xF0000000)+0x2000000+((addr &0xFFFFF)<<5)+(bitnum<<2)) 
+#define MEM_ADDR(addr)  *((volatile unsigned long  *)(addr)) 
+#define BIT_ADDR(addr, bitnum)   MEM_ADDR(BITBAND(addr, bitnum)) 
+//IO口地址映射
+#define GPIOA_ODR_Addr    (GPIOA_BASE+12) //0x4001080C 
+#define GPIOB_ODR_Addr    (GPIOB_BASE+12) //0x40010C0C 
+#define GPIOC_ODR_Addr    (GPIOC_BASE+12) //0x4001100C 
+#define GPIOD_ODR_Addr    (GPIOD_BASE+12) //0x4001140C 
+#define GPIOE_ODR_Addr    (GPIOE_BASE+12) //0x4001180C 
+#define GPIOF_ODR_Addr    (GPIOF_BASE+12) //0x40011A0C    
+#define GPIOG_ODR_Addr    (GPIOG_BASE+12) //0x40011E0C    
+
+#define GPIOA_IDR_Addr    (GPIOA_BASE+8) //0x40010808 
+#define GPIOB_IDR_Addr    (GPIOB_BASE+8) //0x40010C08 
+#define GPIOC_IDR_Addr    (GPIOC_BASE+8) //0x40011008 
+#define GPIOD_IDR_Addr    (GPIOD_BASE+8) //0x40011408 
+#define GPIOE_IDR_Addr    (GPIOE_BASE+8) //0x40011808 
+#define GPIOF_IDR_Addr    (GPIOF_BASE+8) //0x40011A08 
+#define GPIOG_IDR_Addr    (GPIOG_BASE+8) //0x40011E08 
+ 
+//IO口操作,只对单一的IO口!
+//确保n的值小于16!
+#define PAout(n)   BIT_ADDR(GPIOA_ODR_Addr,n)  //输出 
+#define PAin(n)    BIT_ADDR(GPIOA_IDR_Addr,n)  //输入 
+
+#define PBout(n)   BIT_ADDR(GPIOB_ODR_Addr,n)  //输出 
+#define PBin(n)    BIT_ADDR(GPIOB_IDR_Addr,n)  //输入 
+
+#define PCout(n)   BIT_ADDR(GPIOC_ODR_Addr,n)  //输出 
+#define PCin(n)    BIT_ADDR(GPIOC_IDR_Addr,n)  //输入 
+
+#define PDout(n)   BIT_ADDR(GPIOD_ODR_Addr,n)  //输出 
+#define PDin(n)    BIT_ADDR(GPIOD_IDR_Addr,n)  //输入 
+
+#define PEout(n)   BIT_ADDR(GPIOE_ODR_Addr,n)  //输出 
+#define PEin(n)    BIT_ADDR(GPIOE_IDR_Addr,n)  //输入
+
+#define PFout(n)   BIT_ADDR(GPIOF_ODR_Addr,n)  //输出 
+#define PFin(n)    BIT_ADDR(GPIOF_IDR_Addr,n)  //输入
+
+#define PGout(n)   BIT_ADDR(GPIOG_ODR_Addr,n)  //输出 
+#define PGin(n)    BIT_ADDR(GPIOG_IDR_Addr,n)  //输入
+
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 #define VERSION 1
 #define ERROR_TIME 12
 
@@ -34,57 +78,57 @@
     // CAN 使能开关
     #define CAN1_EN                        1u
     // CAN 端口定义
-    #define CANx_TX_PIN                    GPIO_PIN_12
-    #define CANx_TX_GPIO_PORT              GPIOA
-    #define CANx_RX_PIN                    GPIO_PIN_11
-    #define CANx_RX_GPIO_PORT              GPIOA
+    #define CAN1_Tx_PIN                    GPIO_PIN_12
+    #define CAN1_Tx_GPIO_PORT              GPIOA
+    #define CAN1_Rx_PIN                    GPIO_PIN_11
+    #define CAN1_Rx_GPIO_PORT              GPIOA
     //CAN收 发指示灯
     #define CAN_Tx_LED                     PBout(10)
     #define CAN_Rx_LED                     PBout(11)
     // CAN 时钟参数
-    #define CANx                           CAN1
-    #define CANx_CLK_ENABLE()              __HAL_RCC_CAN1_CLK_ENABLE()       //msp init
-    #define CANx_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define CAN1_CLK_ENABLE()              __HAL_RCC_CAN1_CLK_ENABLE()       //msp init
+    #define CAN1_GPIO_CLK_ENABLE()         __HAL_RCC_GPIOA_CLK_ENABLE()
     // CAN中断
-    #define CANx_RX_IRQn                   USB_LP_CAN1_RX0_IRQn
+    #define CAN1_Rx_SID_IRQn                   USB_LP_CAN1_RX0_IRQn
+    #define CAN1_Rx_EXID_IRQn                  CAN1_RX1_IRQn 
 
 // TIM相关定义
     //TIM 时钟使能
     #define TIM2_EN                        1u
     //TIM 时钟参数
-    #define TIMx                           TIM2
-    #define TIMx_CLK_ENABLE()              __HAL_RCC_TIM2_CLK_ENABLE()
-    //TIM 中断参数
-    #define TIMx_IRQn                      TIM2_IRQn
+    #define TIM2_CLK_ENABLE()              __HAL_RCC_TIM2_CLK_ENABLE()
 
 // USART相关定义
     //USART 使能开关
     #define UART2_EN                      1u
     //USART 引脚参数
-    #define UARTx_Tx_PIN                  GPIO_PIN_2
-    #define UARTx_Tx_GPIO_PORT            GPIOA
-    #define UARTx_Rx_PIN                  GPIO_PIN_3
-    #define UARTx_Rx_GPIO_PORT            GPIOA
+    #define UART2_Tx_PIN                  GPIO_PIN_2
+    #define UART2_Tx_GPIO_PORT            GPIOA
+    #define UART2_Rx_PIN                  GPIO_PIN_3
+    #define UART2_Rx_GPIO_PORT            GPIOA
     //USART 外设参数
-    #define UARTx                         USART2
-    #define UARTx_CLK_ENABLE              __HAL_RCC_USART2_CLK_ENABLE()       //msp init
-    #define UARTx_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define UART2_CLK_ENABLE              __HAL_RCC_USART2_CLK_ENABLE()       //msp init
+    #define UART2_GPIO_CLK_ENABLE()       __HAL_RCC_GPIOA_CLK_ENABLE()
+    #define UART2_DMA_CLK_ENABLE()        __HAL_RCC_DMA1_CLK_ENABLE()
     //USART 中断参数
-    #define UARTx_IRQn                    USART2_IRQn
-    //USART 接收参数
-    #define UART_RxDataSize               17u
+    #define UART2_IRQn                    USART2_IRQn
+    #define UART2_DMA_Tx_IRQn             DMA1_Channel7_IRQn
+    #define UART2_DMA_Rx_IRQn             DMA1_Channel6_IRQn
+    //USART 参数
+    #define UART2_RxDataSize               17u
+    #define UART2_TxDataSize               8u
 
 //结构体定义
 	typedef struct TakeOver_Structure
 	{
-		u8 duration;        //点灯持续时间
-		u8 cmd[15];         //点灯状态 共120bits 前60位   为0-4号灯控板，后60位为5-9号灯控板
+		uint8_t Duration;        //点灯持续时间
+		uint8_t Cmd[15];         //点灯状态 共120bits 前60位   为0-4号灯控板，后60位为5-9号灯控板
 	} TakeOverStructure;
 	
 	typedef struct Opt_Light_Reply_Structure
 	{
 		uint8_t ReplyState;          //灯控板是否反馈点灯信息 0：无  1：有
-		uint8_t data[8];             //灯控板反馈的8字节点灯信息存储数组
+		uint8_t Data[8];             //灯控板反馈的8字节点灯信息存储数组
 		
 		uint8_t LightColour[4];     //灯组（通道）当前灯色
 		uint8_t LightState[4];      //灯组状态
@@ -109,7 +153,7 @@
 	typedef struct Opt_Light_Reply_ResultCount
 	{
 		uint8_t Check_Enable;		//是否需要检测
-		uint8_t	NoReplyTimes;       //需要检查，但没有收到灯控板回复点灯状态的次数
+		uint8_t	No_Of_Reply_Times;       //需要检查，但没有收到灯控板回复点灯状态的次数
 
 		uint8_t	Red_Received_RV_times[4];       //点红灯时，有红灯电压计数
 		uint8_t	Yellow_Received_YV_times[4];    //点黄灯时，有黄灯电压计数
